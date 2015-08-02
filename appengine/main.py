e4d3875af0d612fc66a7b9535745dd7e753cc650
ninja_td <tanmay.dehury@gmail.com>
2015-08-02 12:08:26 -0700

    fixes to the diff to escape html

--- appengine/main.py
+++ appengine/main.py
@@ -2,8 +2,11 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
+    if self.request.path == '/diff':
+      self.redirect('/s/diff.html')
+      return
     self.redirect('/s/notepad.html')
 
 app = webapp2.WSGIApplication([
-  ('/', MainPage),
+  ('/.*', MainPage),
   ], debug=True)
